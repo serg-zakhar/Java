371f212b1eac19133078b5d38d411468ff71d862
SergZakhar <szakhar@gmail.com>
2023-04-15 11:43:15 +0300

    Merge with note

--- Lesson1/Program.java
+++ Lesson1/Program.java
@@ -1,10 +1,12 @@
 package Lesson1;
 
-/**
- * Program
- */
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Program {
+
     public static void main(String[] args) {
-        System.out.println("Hello"); 
+        ArrayList arr = new ArrayList();
+
     }
 }
