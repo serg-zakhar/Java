2827228832688a2a8da79264fea495ef7f45a6dd
serg <serg@inf-03158>
2023-05-12 11:17:54 +0300

    HW3 1.1

---
+++ OOP/Lesson3/HW3.java
@@ -0,0 +1,144 @@
+package OOP.Lesson3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Random;
+
+public class HW3 {
+
+    private static Random random = new Random();
+
+    static Employee generateEmployee(){
+        String[] names = new String[] { "Анатолий", "Глеб", "Клим", "Мартин", "Лазарь", "Владлен", "Клим", "Панкратий", "Рубен", "Герман" };
+        String[] surnames = new String[] { "Григорьев", "Фокин", "Шестаков", "Хохлов", "Шубин", "Бирюков", "Копылов", "Горбунов", "Лыткин", "Соколов" };
+
+        int workerSalary = random.nextInt(200, 300);
+        int freelancerSalary = 1500;
+        int index = random.nextInt(30, 50);
+        int hours = random.nextInt(20,40);
+        int typeWorker = random.nextInt(0,2);
+        int age = random.nextInt(20,40);
+        // System.out.println(typeWorker);
+
+        if (typeWorker == 1) {
+            return new Worker(names[random.nextInt(10)], surnames[random.nextInt(10)], workerSalary * index, age);
+        } else {
+            return new Freelancer(names[random.nextInt(10)], surnames[random.nextInt(10)], freelancerSalary, age, hours);
+        }
+    }
+    public static void main(String[] args) {
+
+        // Worker worker = new Worker("Анатолий", "Шестаков", 80000);
+        // System.out.println(worker);
+
+        Employee[] employees = new Employee[10];
+        for (int i = 0; i < employees.length; i++)
+            employees[i] = generateEmployee();
+
+        for (Employee employee : employees){
+            System.out.println(employee);
+        }
+
+        //Arrays.sort(employees, new NameComparator());
+        Arrays.sort(employees);
+
+        System.out.printf("\n*** Отсортированный массив сотрудников ***\n\n");
+
+        for (Employee employee : employees){
+            System.out.println(employee);
+        }
+
+    }
+    
+}
+
+class SalaryComparator implements Comparator<Employee> {
+
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+
+        //return o1.calculateSalary() == o2.calculateSalary() ? 0 : (o1.calculateSalary() > o2.calculateSalary() ? 1 : -1);
+        return Double.compare( o2.calculateSalary(), o1.calculateSalary());
+    }
+}
+
+class NameComparator implements Comparator<Employee> {
+
+
+    @Override
+    public int compare(Employee o1, Employee o2) {
+        //return o1.calculateSalary() == o2.calculateSalary() ? 0 : (o1.calculateSalary() > o2.calculateSalary() ? 1 : -1);
+        int res = o1.name.compareTo(o2.name);
+        if (res == 0){
+            res = o1.surName.compareTo(o2.surName);
+        }
+        return res;
+    }
+}
+
+abstract class Employee implements Comparable<Employee>{
+
+    protected String name;
+    protected String surName;
+    protected double salary;
+    protected int age;
+
+    public Employee(String name, String surName, double salary, int age) {
+        this.name = name;
+        this.surName = surName;
+        this.salary = salary;
+        this.age = age;
+    }
+
+    public abstract  double calculateSalary();
+
+    @Override
+    public String toString() {
+        return String.format("Сотрудник: %s %s, возраст %d лет: Среднемесячная заработная плата: %.2f", name, surName, age, salary);
+    }
+
+    @Override
+    public int compareTo(Employee o) {
+        return Double.compare( o.calculateSalary(), calculateSalary());
+    }
+
+}
+
+class Worker extends Employee {
+
+    public Worker(String name, String surName, double salary, int age) {
+        super(name, surName, salary, age);
+    }
+
+    @Override
+    public double calculateSalary() {
+        return salary ;
+        //TODO: Для фрилансера - return 20 * 5 * salary
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s; Рабочий; Среднемесячная заработная плата (фиксированная месячная оплата): %.2f (руб.)", name, surName, salary);
+    }
+}
+
+class Freelancer extends Employee{
+
+    int hours;
+
+    public Freelancer(String name, String surName, double salary, int age, int hours) {
+        super(name, surName, salary, age);
+        this.hours = hours;
+    }
+
+    @Override
+    public double calculateSalary() {
+        return salary * hours;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s; Фрилансер; Среднемесячная заработная плата (оплата за отработанное время): %.2f (руб.)", name, surName, calculateSalary());
+    }
+}
